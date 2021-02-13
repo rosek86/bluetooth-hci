60ef962bcf558c2d77e8db97a4340d3604c331f6
Krzysztof Rosinski <rosek86@gmail.com>
2021-02-13 05:48:23 +0100

    add slip layer

---
+++ src/Slip.ts
@@ -0,0 +1,92 @@
+enum SlipDecoderState {
+  WaitBeg, WaitEnd, WaitEndEsc,
+}
+
+export class Slip {
+  private readonly END     = 0x0C; // indicates end of packet
+  private readonly ESC     = 0xDB; // indicates byte stuffing
+  private readonly ESC_END = 0xDC; // ESC ESC_END means END data byte
+  private readonly ESC_ESC = 0xDD; // ESC ESC_ESC means ESC data byte
+
+  private decoderState = SlipDecoderState.WaitBeg;
+  private decoderData: number[] = [];
+
+  public encode(data: Buffer): Buffer {
+    const encoded = [this.END];
+
+    for (const byte of data) {
+      switch (byte) {
+        case this.END:
+          encoded.push(this.ESC);
+          encoded.push(this.ESC_END);
+          break;
+        case this.ESC:
+          encoded.push(this.ESC);
+          encoded.push(this.ESC_ESC);
+          break;
+        default:
+          encoded.push(byte);
+          break;
+      }
+    }
+
+    encoded.push(this.END);
+    return Buffer.from(encoded);
+  }
+
+  public resetDecoder(): void {
+    this.decoderState = SlipDecoderState.WaitBeg;
+    this.decoderData = [];
+  }
+
+  public decode(data: Buffer): Buffer[] {
+    const result: Buffer[] = [];
+
+    for (const byte of data) {
+      switch (this.decoderState) {
+        case SlipDecoderState.WaitBeg:
+          switch (byte) {
+            case this.END:
+              this.decoderState = SlipDecoderState.WaitEnd;
+              this.decoderData = [];
+              break;
+            default:
+              // skip
+              break;
+          }
+          break;
+
+        case SlipDecoderState.WaitEnd:
+          switch (byte) {
+            case this.ESC:
+              this.decoderState = SlipDecoderState.WaitEndEsc;
+              break;
+            case this.END:
+              if (this.decoderData.length > 0) {
+                result.push(Buffer.from(this.decoderData));
+                this.decoderData = [];
+              }
+              break;
+            default:
+              this.decoderData.push(byte);
+              break;
+          }
+          break;
+
+        case SlipDecoderState.WaitEndEsc:
+          switch (byte) {
+            case this.ESC_END:
+              this.decoderData.push(this.END);
+              break;
+            case this.ESC_ESC:
+              this.decoderData.push(this.ESC);
+              break;
+          }
+          this.decoderState = SlipDecoderState.WaitEnd;
+          break;
+      }
+    }
+
+    return result;
+  }
+}
