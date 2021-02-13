7b1c0bbdeffce2ae60e0f4587908e6c726c3ef29
Krzysztof Rosinski <kr.rosinski@gmail.com>
2021-02-13 17:14:40 +0100

    h5 decoding

--- src/H5.ts
+++ src/H5.ts
@@ -1,4 +1,28 @@
+interface H5Header {
+  seqNum: number;
+  ackNum: number;
+  crcPresent: 0|1;
+  reliablePacket: 0|1;
+  packetType: number;
+  payloadLength: number;
+}
+
+type H5Packet = H5Header & { payload?: number[]; };
+
+enum H5TransportRetCode {
+  ParserSlipPayloadSize           = 1,
+  ParserSlipCalculatedPayloadSize = 2,
+  ParserHeaderChecksum            = 3,
+  ParserPacketChecksum            = 4,
+}
+
+type DecoderResult = (
+  { code: 0 } & H5Packet) |
+  { code: H5TransportRetCode };
+
 export class H5 {
+  private readonly h5HeaderLength = 4;
+
   private readonly seqNumMask                     = 0x07;
   private readonly seqNumPos                      = 0;
   private readonly ackNumMask                     = 0x07;
@@ -36,14 +60,7 @@ export class H5 {
     }
   }
 
-  private addHeader(packet: number[], hdr: {
-    seqNum: number,
-    ackNum: number,
-    crcPresent: 0|1,
-    reliablePacket: 0|1,
-    packetType: number,
-    payloadLength: number,
-  }): void {
+  private addHeader(packet: number[], hdr: H5Header): void {
     packet.push(
       ((hdr.seqNum         & this.seqNumMask        ) << this.seqNumPos        ) |
       ((hdr.ackNum         & this.ackNumMask        ) << this.ackNumPos        ) |
@@ -72,7 +89,7 @@ export class H5 {
     return checksum & 0xFF;
   }
 
-  private addCrc16(packet: number[]) {
+  private addCrc16(packet: number[]): void {
     const crc16 = this.calculateCrc16Checksum(packet);
     packet.push((crc16 >> 0) & 0xFF);
     packet.push((crc16 >> 8) & 0xFF);
@@ -88,5 +105,65 @@ export class H5 {
       crc ^= (crc & 0xFF) << 5;
     }
     return crc & 0xFFFF;
+  }
+
+  public decode(slipPayload: number[]): DecoderResult {
+    if (slipPayload.length < this.h5HeaderLength) {
+      return { code: H5TransportRetCode.ParserSlipPayloadSize };
+    }
+
+    const seqNum         = ((slipPayload[0] >> this.seqNumPos        ) & this.seqNumMask);
+    const ackNum         = ((slipPayload[0] >> this.ackNumPos        ) & this.ackNumMask);
+    const crcPresent     = ((slipPayload[0] >> this.crcPresentPos    ) & this.crcPresentMask);
+    const reliablePacket = ((slipPayload[0] >> this.reliablePacketPos) & this.reliablePacketMask);
+    const packetType     = ((slipPayload[1] >> this.packetTypePos    ) & this.packetTypeMask);
+    const payloadLength  = (
+      ((slipPayload[1] >> this.payloadLengthPos) & this.payloadLengthFirstNibbleMask) |
+      ((slipPayload[2] << this.payloadLengthPos) & 0xFF)
+    );
+    const headerChecksum = slipPayload[3];
+
+    const result: DecoderResult = {
+      code: 0,
+      seqNum,
+      ackNum,
+      crcPresent: crcPresent ? 1:0,
+      reliablePacket: reliablePacket ? 1:0,
+      packetType,
+      payloadLength,
+    };
+
+    // Check if received packet size matches the packet size stated in header
+    const calculatedPayloadSize = payloadLength + this.h5HeaderLength + (crcPresent ? 2 : 0);
+
+    if (slipPayload.length !== calculatedPayloadSize) {
+      return { code: H5TransportRetCode.ParserSlipCalculatedPayloadSize };
+    }
+
+    const calculatedHeaderChecksum = this.calculateHeaderChecksum(slipPayload);
+
+    if (headerChecksum !== calculatedHeaderChecksum) {
+      return { code: H5TransportRetCode.ParserHeaderChecksum };
+    }
+
+    if (crcPresent) {
+      const packetChecksum = slipPayload[payloadLength + this.h5HeaderLength] +
+                              (slipPayload[payloadLength + this.h5HeaderLength + 1] << 8);
+      const calculatedPacketChecksum = this.calculateCrc16Checksum(
+        slipPayload.slice(0, payloadLength + this.h5HeaderLength)
+      );
+
+      if (packetChecksum !== calculatedPacketChecksum) {
+        return { code: H5TransportRetCode.ParserPacketChecksum };
+      }
+    }
+
+    if (payloadLength > 0) {
+      result.payload = slipPayload.slice(
+        this.h5HeaderLength, this.h5HeaderLength + payloadLength
+      );
+    }
+
+    return result;
   }
 }
