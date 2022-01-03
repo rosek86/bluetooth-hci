e29847f89c61e0e1b0ff6cffe1e89db41085a943
Krzysztof Rosinski <rosek86@gmail.com>
2022-01-03 22:52:58 +0100

    extract hci device setup to utils class

--- examples/advertising.ts
+++ examples/advertising.ts
@@ -21,66 +21,7 @@ import {
     });
     const hci = adapter.Hci;
 
-    await hci.reset();
-
-    const localFeatures = await hci.readLocalSupportedFeatures();
-    if (localFeatures.leSupported === false) {
-      throw new Error('LE not supported');
-    }
-
-    const localVersion = await hci.readLocalVersionInformation();
-    console.log('Local Version', localVersion);
-
-    const bdAddress = await hci.readBdAddr();
-    console.log('BD Address:', bdAddress.toString());
-
-    const leTransmitPower = await hci.leReadTransmitPower();
-    console.log(`LE Transmit Power:`, leTransmitPower);
-
-    const leBufferSize = await hci.leReadBufferSize();
-    console.log('LE Buffer Size:', leBufferSize);
-
-    const leFeatures = await hci.leReadLocalSupportedFeatures();
-    console.log('LE Features:', leFeatures);
-
-    const leStates = await hci.leReadSupportedStates();
-    console.log('LE States:', leStates);
-
-    const localCommands = await hci.readLocalSupportedCommands();
-
-    console.log('Supported commands:')
-    for (const [key, value] of Object.entries(localCommands)) {
-      if (value === true) {
-        console.log(key);
-      }
-    }
-
-    await hci.setEventMask({
-      disconnectionComplete:                true,
-      encryptionChange:                     true,
-      encryptionKeyRefreshComplete:         true,
-      readRemoteVersionInformationComplete: true,
-      leMeta:                               true,
-    });
-    await hci.setEventMaskPage2({});
-    await hci.leSetEventMask({
-      connectionComplete:                   false,
-      advertisingReport:                    false,
-      connectionUpdateComplete:             true,
-      readRemoteFeaturesComplete:           true,
-      longTermKeyRequest:                   true,
-      remoteConnectionParameterRequest:     true,
-      dataLengthChange:                     true,
-      readLocalP256PublicKeyComplete:       true,
-      generateDhKeyComplete:                true,
-      enhancedConnectionComplete:           true,
-      directedAdvertisingReport:            true,
-      phyUpdateComplete:                    true,
-      extendedAdvertisingReport:            true,
-      scanTimeout:                          true,
-      advertisingSetTerminated:             true,
-      channelSelectionAlgorithm:            true,
-    });
+    await Utils.defaultAdapterSetup(hci);
 
     const advSets = await hci.leReadNumberOfSupportedAdvertisingSets();
     console.log(`Number of supported advertising sets: ${advSets}`);
