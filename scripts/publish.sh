312992fc6effc15a8196f981f33fc7c8b0de38b6
Krzysztof Rosiński <kr.rosinski@gmail.com>
2023-11-16 21:50:54 +0100

    1.0.2 release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-# Create a temporary directory
-TMP_DIR=$(mktemp -d)
-echo $TMP_DIR
-
 rm -fr lib/
 npm run build.prod
 
-# Copy the contents of /lib to the temporary directory
-cp -r lib/* $TMP_DIR/
-
-# Optionally, copy other necessary files like package.json, README.md, etc.
-cp package.json $TMP_DIR/
-cp README.md $TMP_DIR/
-cp LICENSE $TMP_DIR/
-cp CHANGELOG.md $TMP_DIR/
-
-# Change to the temporary directory and publish
-(
-  cd $TMP_DIR
-  npm publish
-)
-
-# Clean up: Delete the temporary directory
-rm -rf $TMP_DIR
+npm publish
