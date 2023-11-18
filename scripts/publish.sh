3505c7d7129ac61abf1df801694ac8c16937f197
Krzysztof Rosiński <kr.rosinski@gmail.com>
2023-11-18 10:05:18 +0100

    publish nrf52840dk firmware

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,5 +6,6 @@ npm run release
 # Copy hex files to dist
 mkdir -p dist/hex
 cp zephyr/hci_uart/nrf52840dongle_nrf52840.hex dist/hex
+cp zephyr/hci_uart/nrf52840dk_nrf52840.hex dist/hex
 
 npm publish
